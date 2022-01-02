756e95ea4661a30ccc68e9c2d7c250847696db43
Jadid Alif Ramadhan <jadidalif123@gmail.com>
2022-01-02 14:00:44 +0700

    Database dan index
    menambahkan database dan update index

--- index.php
+++ index.php
@@ -5,8 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Mahasiswa</title>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+<!-- navbar -->
+<nav class="navbar navbar-dark bg-danger flex-md-nowrap p-2 shadow col-12">
+    <div class="container-fluid">
+        <a class="navbar-brand h1">
+            <span class="navbar-brand mb-0 h1">Data Mahasiswa</span>
+        </a>
+    </div>
+</nav>
+<div class="container-fluid">
+		<div class="row">
+			<!-- Sidebar -->
+			<div class="col-md-2 d-none d-md-block bg-light sidebar" style="height: 100vh">
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<li class="nav-item mt-2 active">
+							<a class="nav-link" href="">
+								Mahasiswa
+							</a>
+						</li>
+						<div class="dropdown-divider"></div>
+						<li class="nav-item mt-2">
+							<a class="nav-link" href="">
+								Dosen
+							</a>
+						</li>
+						<div class="dropdown-divider"></div>
+						<li class="nav-item mt-2">
+							<a class="nav-link" href="">
+								Prodi
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+        </div>
+    </div>
 </body>
 </html>
