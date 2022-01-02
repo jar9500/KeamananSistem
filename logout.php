a75552c79e9e1ebf20eac844a4ec2177f158a4d5
19027_luckyanisjanuarsigit <83755222+luckyanisjs@users.noreply.github.com>
2022-01-02 19:34:15 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 
 unset($_SESSION["Nama"]);
-$_SESSION["alert"] = "Anda telah Logout";
+$_SESSION["alert"] = "Perhatian! Anda Telah Logout";
 session_destroy();
 header("Location:index.php");
 ?>
